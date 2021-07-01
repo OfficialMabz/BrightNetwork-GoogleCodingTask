e3a7714a272a06bbe62f09892cdaf507dce43dd8
Mahbub Rahman <45140799+OfficialMabz@users.noreply.github.com>
2021-07-01 21:50:04 +0100

    Finished task 3

--- Youtube-BN/google-code-sample/python/src/video_player.py
+++ Youtube-BN/google-code-sample/python/src/video_player.py
@@ -38,6 +38,8 @@ class VideoPlayer:
     
     
     def play_video(self, video_id):
+        """Plays a video"""
+
         videos = self._video_library.get_video(video_id)
         
         #if already a video playing stop and start the new one
@@ -54,18 +56,27 @@ class VideoPlayer:
 
 
     def stop_video(self):
+        #checks if the global variable has any video playing 
+        #if it is then it stops playing and removes from the dictionary
         if (len(self.video_playing) > 0):
             print(f"Stopping video: {self.video_playing.popitem()[0]}")
         else:
             print('Cannot stop video: No video is currently playing')
 
     def play_random_video(self):
+
+        """Plays a random video from the video library."""
+
         allvideos = self._video_library.get_all_videos()
         alltitles = []
+        #titles of all videos are inserted in a list
         for x in range(len(allvideos)):
             alltitles.append(allvideos[x].title)
-        
+
+        #this will choose a titles from the list
         randchoice = random.choice(alltitles)
+
+        #below after some validation checks the random title is played and other video is stopped
         if (len(self.video_playing) > 0):
             print(f"Stopping video: {self.video_playing.popitem()[0]}")
             print(f"Playing video: {randchoice}")
@@ -74,11 +85,11 @@ class VideoPlayer:
             print(f"Playing video: {randchoice}")
             self.video_playing[randchoice] = 'playing'
 
-        """Plays a random video from the video library."""
 
     def pause_video(self):
+        #here we extract the value and key in order to check the status of video
+        #if it is playing, we pause otherwise we show message
         if bool(self.video_playing):
-            #item = self.video_playing
             val = list(self.video_playing.values()).pop()
             ky = list(self.video_playing.keys()).pop()
             if (val != 'pause'):
@@ -91,9 +102,10 @@ class VideoPlayer:
 
     def continue_video(self):
         """Resumes playing the current video."""
-
+        
+        #this is similar to previous example as we also extract the key and value from dictionary
+        #to check the video status and change to 'playin' when continued
         if bool(self.video_playing):
-            #item = self.video_playing
             val = list(self.video_playing.values()).pop()
             ky = list(self.video_playing.keys()).pop()
             if (val == 'pause'):
@@ -107,16 +119,21 @@ class VideoPlayer:
 
     def show_playing(self):
         """Displays video currently playing."""
+        #this is where we also do similar thing as previously done to check the process
+        #we also get all the videos to extract the videos information to display to user
         if bool(self.video_playing):
             #item = self.video_playing
             val = list(self.video_playing.values()).pop()
             ky = list(self.video_playing.keys()).pop()
+            #gets all the videos
             videos = self._video_library.get_all_videos()
             for i in range(len(videos)):
+                #below four line removes the extra characters to strip the tags
                 tag = str(videos[i].tags)
                 characters_to_remove = "()'',"
                 for character in characters_to_remove:
                     tag = tag.replace(character, "")
+                #this check is to see whether the video is paused to playing
                 if videos[i].title == ky and val == 'pause':
                     print(f'Currently playing: {videos[i].title} ({videos[i].video_id}) [{tag}] - PAUSED')
                 elif videos[i].title == ky:
@@ -131,6 +148,8 @@ class VideoPlayer:
         Args:
             playlist_name: The playlist name.
         """
+        #this is where we use our second global dictionary to store the playlists and the videos within it
+        #below line makes the playlist uppercase to compare with user input for validation
         ky = list(self.playLists.keys())
         ky = [each_string.upper() for each_string in ky]
         if playlist_name.isspace():
@@ -138,6 +157,7 @@ class VideoPlayer:
         elif playlist_name.upper() in ky:
             print("Cannot create playlist: A playlist with the same name already exists")
         else:
+            #creates the playlists with the given name
             self.playLists[playlist_name] = []
             print(f"Successfully created new playlist: {playlist_name}")
 
@@ -148,7 +168,8 @@ class VideoPlayer:
             playlist_name: The playlist name.
             video_id: The video_id to be added.
         """
-
+        #we do similar thing as before by retrieving the playlist keys and checking with the user input for validation
+        #we add the video_id of the video to the list as a value, so it can be used to retrieve video information with get_video() function
         playlists = list(self.playLists.keys())
         playlists = [each_string.upper() for each_string in playlists]
         vid = self._video_library.get_video(video_id)
@@ -157,6 +178,8 @@ class VideoPlayer:
         elif not bool(vid):
             print(f"Cannot add video to {playlist_name}: Video does not exist")
         else:
+            #below checks are done to check for case sensitivity of given and stored playlist
+            # if it matches we add it to the playlist 
             for keys in self.playLists:
                 if keys.upper() == playlist_name.upper():
                     if video_id not in self.playLists[keys]:
@@ -168,6 +191,7 @@ class VideoPlayer:
     def show_all_playlists(self):
         
         """Display all playlists."""
+        #below two lines gets the keys and sorts them in alphabetic order regardless of their case sensitivity
         playlists = list(self.playLists.keys())
         playlists = sorted(playlists, key=lambda k: (k.lower(), k.islower()))
         if len(playlists) <= 0:
@@ -302,7 +326,35 @@ class VideoPlayer:
         Args:
             video_tag: The video tag to be used in search.
         """
-        print("search_videos_tag needs implementation")
+        videos = self._video_library.get_all_videos()
+        tag_tracker = []
+        for x in range(len(videos)):
+            check = list(videos[x].tags)
+            out = map(lambda x:x.upper(), check)
+            output = list(out)
+            if video_tag.upper() in output:
+                tag_tracker.append(videos[x].title)
+        vid_tracker = sorted(tag_tracker)
+        if len(vid_tracker) <= 0:
+            print(f"No search results for {video_tag}")
+        else:
+            print(f"Here are the results for {video_tag}:")
+            for i in range(len(vid_tracker)):
+                for k in range(len(videos)):
+                    if videos[k].title == vid_tracker[i]:
+                        tag = str(videos[k].tags)
+                        characters_to_remove = "()'',"
+                        for character in characters_to_remove:
+                            tag = tag.replace(character, "")
+                        print(f"{i+1}) {videos[k].title} ({videos[k].video_id}) [{tag}] ")
+            print("Would you like to play any of the above? If yes, specify the number of the video. \n If your answer is not a valid number, we will assume it's a no.")
+            choice = input()
+            for i in range(len(vid_tracker)):
+                if choice.isdigit():
+                    if int(choice) == i+1:
+                        print(f"Playing video: {vid_tracker[i]}")
+                        self.video_playing[vid_tracker[i]] = 'playing'
+
 
     def flag_video(self, video_id, flag_reason=""):
         """Mark a video as flagged.
